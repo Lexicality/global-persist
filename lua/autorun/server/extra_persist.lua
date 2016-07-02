e25278e68260a5f6bf3efaf33cb5c7d4cc3f2243
Lex Robinson <lexi@lexi.org.uk>
2016-07-02 16:30:58 +0100

    Make persistence persist through clenaups

---
+++ lua/autorun/server/extra_persist.lua
@@ -0,0 +1,22 @@
+--[[
+	Extra Persistence
+	Copyright (c) 2016 Lex Robinson
+	This code is freely available under the MIT License
+--]]
+local cvar = CreateConVar(
+	"sbox_persist_cleanup",
+	"",
+	0,
+	"Set to anything but 0 to enable persisting through cleanups"
+);
+
+hook.Add("PostCleanupMap", "Global Persistence - Extra Persistence", function()
+
+	if (cvar:GetString() == "0") then return end
+
+	local PersistPage = GetConVarString( "sbox_persist" )
+	if ( PersistPage == "0" ) then return end
+
+	hook.Run( "PersistenceLoad", PersistPage )
+
+end)
